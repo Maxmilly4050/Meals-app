4f3af85034cdf9b22cfc57b87e4dd9f2aa04054f
Maxmilly4050 <maxsteel4050@gmail.com>
2026-01-23 13:17:05 +0300

    Multi screens navigation implementation

---
+++ lib/screens/meals.dart
@@ -0,0 +1,59 @@
+import 'package:flutter/material.dart';
+import 'package:meals_app/models/meal.dart';
+
+
+class MealsScreen extends StatelessWidget {
+  const MealsScreen({super.key, required this.title, required this.meals});
+
+  final String title;
+  final List<Meal> meals;
+
+  @override
+  Widget build(BuildContext context) {
+    Widget content;
+
+      if (meals.isEmpty) {
+        content =  Center(
+          child: Column(
+            mainAxisSize: MainAxisSize.min,
+            children: [
+              Text(
+                'Uh oh ... nothing here!',
+                style: Theme.of(context).textTheme.headlineLarge!.copyWith(
+                      color: Theme.of(context).colorScheme.onSurface.withAlpha(150),
+              ),
+              ),
+      SizedBox(height: 16),
+              Text(
+                'Try selecting a different category!',
+                style: Theme.of(context).textTheme.bodyLarge!.copyWith(
+                      color: Theme.of(context).colorScheme.onSurface.withAlpha(150),
+              ),
+              ),
+            ],
+          ),
+        );
+      }else {
+        content = ListView.builder(
+          itemCount: meals.length,
+          itemBuilder: (ctx, index) {
+            final meal = meals[index];
+            return ListTile(
+              title: Text(meal.title),
+              leading: CircleAvatar(
+                backgroundImage: NetworkImage(meal.imageUrl),
+              ),
+            );
+          },
+        );
+      }
+
+    return Scaffold(
+      appBar: AppBar(
+        title: Text(title),
+      ),
+      body: content,
+    );
+  }
+}
+
---
+++ lib/widgets/category_grid_item.dart
@@ -0,0 +1,47 @@
+import 'package:flutter/material.dart';
+import 'package:meals_app/models/category.dart';
+
+class CategoryGridItem extends StatelessWidget {
+  final Category category;
+  final void Function() onSelectCategory;
+
+  const CategoryGridItem({
+    super.key,
+    required this.category,
+    required this.onSelectCategory,
+  });
+
+
+
+  @override
+  Widget build(BuildContext context) {
+    return InkWell(
+      onTap: (){
+        onSelectCategory();
+      },
+      splashColor: Theme.of(context).colorScheme.primary,
+      borderRadius: BorderRadius.circular(15),
+      child: Container(
+        padding: const EdgeInsets.all(20),
+        decoration: BoxDecoration(
+          gradient: LinearGradient(
+            colors: [
+              category.color.withValues(alpha: 0.2),
+              category.color.withValues(alpha: 0.7),
+              category.color,
+            ],
+            begin: Alignment.topLeft,
+            end: Alignment.bottomRight,
+          ),
+          // borderRadius: BorderRadius.circular(15),
+        ),
+        child: Text(
+            category.title,
+            style: Theme.of(context).textTheme.titleLarge!.copyWith(
+              color: Theme.of(context).colorScheme.onSurface,
+            ),
+          ),
+          ),
+    );
+  }
+}
