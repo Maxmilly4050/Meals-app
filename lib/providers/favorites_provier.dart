2959f1f8d7eaf9ea13ac4f5f30e3fb103633425f
Maxmilly4050 <maxsteel4050@gmail.com>
2026-02-11 15:52:06 +0300

    Passing filtered values across multiple screens

---
+++ lib/providers/favorites_provier.dart
@@ -0,0 +1,21 @@
+import 'package:flutter_riverpod/legacy.dart';
+import 'package:meals_app/models/meal.dart';
+
+class FavoriteMealsProvider extends StateNotifier<List<Meal>> {
+  FavoriteMealsProvider() : super([]);
+
+  void toggleMealFavoriteStatus(Meal meal) {
+    final mealIsFavorite = state.contains(meal);
+
+    if(mealIsFavorite){
+      state = state.where((m) => m.id != meal.id).toList();
+    } else {
+      state = [...state, meal];
+    }
+  }
+
+}
+
+final favoriteMealsProvider = StateNotifierProvider<FavoriteMealsProvider, List<Meal>>(
+    (ref) => FavoriteMealsProvider()
+);
